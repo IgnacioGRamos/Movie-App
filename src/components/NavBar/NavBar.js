1643053a2077dfebb0819ca72cf358a20b8a43c2
IgnacioGRamos <ignaciornacho98@gmail.com>
2022-03-18 23:48:19 -0300

    Commit 02

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Logo from '../../logoHenry.png'
 
 import './Navbar.css';
 
@@ -8,7 +7,7 @@ export default function NavBar() {
     return (
         <header className="navbar">
             <div>
-                <img id="logoHenry" src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" />
+                <div>Movie App</div>
             </div>
             <nav>
                 <ul className="list">
